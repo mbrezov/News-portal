adf7aa35ef2e01d7f305097c5a28393860383374
mbrezov <mariobrezovecki@gmail.com>
2023-06-29 01:40:02 +0200

    searchbar styling added

--- src/components/SearchLine.js
+++ src/components/SearchLine.js
@@ -3,8 +3,13 @@ import "./SearchLine.scss";
 const SearchLine = () => {
   return (
     <div className="searchline-container">
-      <div>MyNews</div>
+      <div>
+        My<span>News</span>
+      </div>
       <input className="searchbar" type="search" placeholder="Search news" />
+      <button type="submit">
+        <i className="searchbar-button"></i>
+      </button>
     </div>
   );
 };
