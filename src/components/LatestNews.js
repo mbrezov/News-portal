57987c0596ad5ae34d61015c31d027caff04d3c9
mbrezov <mariobrezovecki@gmail.com>
2023-07-10 01:38:46 +0200

    scrollbar styling updated

--- src/components/LatestNews.js
+++ src/components/LatestNews.js
@@ -34,7 +34,7 @@ const LatestNews = () => {
     container.addEventListener("scroll", handleScroll);
     return () => container.removeEventListener("scroll", handleScroll);
   }, []);
-
+  console.log(limit);
   return (
     <div className="latestnews-container" id="latestnews-container">
       <div className="latest-news-header">
@@ -70,8 +70,8 @@ const LatestNews = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            fillRule="evenodd"
+            clipRule="evenodd"
             d="M4.48025 0.451931C4.30001 0.271689 4.00778 0.271689 3.82754 0.451931L3.338 0.941467C3.15776 1.12171 3.15776 1.41394 3.338 1.59418L7.66256 5.91874C7.70762 5.9638 7.70762 6.03686 7.66256 6.08192L3.33833 10.4061C3.15809 10.5864 3.15809 10.8786 3.33833 11.0589L3.82754 11.5481C4.00778 11.7283 4.30001 11.7283 4.48025 11.5481L9.37561 6.65271C9.73609 6.29223 9.73609 5.70777 9.37561 5.34729L4.48025 0.451931Z"
             fill="#1D1D1B"
           />
